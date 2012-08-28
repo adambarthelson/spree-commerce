e813f1e83b8f82cfcebf0475286039978aea5f40
Steven Hsu <stevenhsu0@gmail.com>
2012-08-28 12:04:20 -0700

    Rails 3.2.7 new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stackato::Application.config.secret_token = '2a4fbcd716b46d56d76ba22ef57c557df723d2eccdfb30711bf5458402c116b103905627791839d89013e59812e9093162e1f1ee3d5a34551f12b681246f043b'
